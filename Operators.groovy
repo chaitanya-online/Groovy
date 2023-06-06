e5dd4cdcaed32f199b359d9978a87e30df0886b0
chaitanya_Nampalli <chaitanyamurali.nampalli@serole.com>
2023-06-06 18:58:54 +0530

    changes

---
+++ Operators.groovy
@@ -0,0 +1,12 @@
+class Operators
+{
+   public  static void main(String[]args){
+    
+      def  x = 10
+      def y =  20 
+      println(x+y)
+      println(x*y)
+      println(x/y)
+      println(x-y)
+    
+}}
