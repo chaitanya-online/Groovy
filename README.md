74c3be4cdbf8fb03bbf421f297068518830dc94c
chaitanya_Nampalli <chaitanyamurali.nampalli@serole.com>
2023-06-06 16:51:17 +0530

    changes

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Basic concepts of groovy
 
+It is case sensitive language
+
+It is dynamically typed language i.e type of variable is resolved at run time
+
 Command to execute groovy is 
 
 
---
+++ Variables.groovy
@@ -0,0 +1,13 @@
+def name = 'Chaitanya'
+println name
+myname = 'Chaitanya'
+println myname
+String secondName = 'Babu'
+println 'Second name is '+ secondName
+dob = '10061999'
+dob = '10-06-1999'
+println "Date of birth is "+dob
+def (a,b,c) = [10,20,30]
+println a
+println b
+println c
