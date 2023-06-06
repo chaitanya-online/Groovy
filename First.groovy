d620c7edfe6993b486cafd8b11b5fda7c6c21f82
chaitanya_Nampalli <chaitanyamurali.nampalli@serole.com>
2023-06-06 16:03:20 +0530

    changes

--- First.groovy
+++ First.groovy
@@ -1,5 +1,12 @@
 class MyFirst {
     static void  main(args) {
+
+        // Single line comment
+        /*
+        ** Multiline
+        Comment
+        
+        */
         println('Hello Chaitanya')
         println('Hello Chaitanya');
         println'Hello Chaitanya'
