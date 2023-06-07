1b27368493acfbbb4e1f4c77e3e86dda41313ae7
chaitanya_Nampalli <chaitanyamurali.nampalli@serole.com>
2023-06-07 10:35:13 +0530

    changes

--- Operators.groovy
+++ Operators.groovy
@@ -3,10 +3,12 @@ class Operators
    public  static void main(String[]args){
     
       def  x = 10
-      def y =  20 
+      def  y =  20 
       println x+y
       println(x*y)
       println(x/y)
       println(x-y)
+
+      assert 1 + 2  == 3   // It won't print anything to console just check the condition . If condition didn't satisfied it throws errors
     
 }}
