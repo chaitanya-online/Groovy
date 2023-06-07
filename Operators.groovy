d8d5bf0abb44e073e925d316f9cd498c36b9b709
chaitanya_Nampalli <chaitanyamurali.nampalli@serole.com>
2023-06-07 12:37:03 +0530

    changes

--- Operators.groovy
+++ Operators.groovy
@@ -10,5 +10,15 @@ class Operators
       println(x-y)
 
       assert 1 + 2  == 3   // It won't print anything to console just check the condition . If condition didn't satisfied it throws errors
+
+  def i = 10;
+  println "value of i before post increment " + i++
+println "value of i after post increment " + i
+
+ def j = 10;
+  println "value of j before pre increment " + ++j
+println "value of j after pre increment " + j
+
+
     
 }}
