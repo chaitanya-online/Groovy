3ea45e0faf35453416397f113d40bc1f58ee4705
chaitanya_Nampalli <chaitanyamurali.nampalli@serole.com>
2023-06-08 12:11:41 +0530

    changes

--- src/Strings.groovy
+++ src/Strings.groovy
@@ -7,10 +7,29 @@ static void main (String[]args)
     println "My name is ".concat(name)
     println "My name is ${name}"
     println "My name is $name"
+	println '=========='
+	
+	def str = / Sky is blue /
+	println str
+	println '=========='
+	def str1 = $/ Sky is blue /$
+	println str
 	
 	def subject = """This is groovy script"""
 	println subject
 	println subject.length()
 	println subject[-0].toLowerCase()
+	println subject.isBlank()
+	println subject.isEmpty()
+println name[1,5,6]
+println "substring " + name.substring(4)  +' substring '+ name.substring(1, 4) 
+
+def language = 'Groovy Script '
+println language.split(" ")
+println language-("Groovy")
+println language*3
+	
+	
 }
+
 }
