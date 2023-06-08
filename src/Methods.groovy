b4c415ec8cfbfe886a5bbcf93433c50c590605d5
chaitanya_Nampalli <chaitanyamurali.nampalli@serole.com>
2023-06-08 12:52:54 +0530

    changes

---
+++ src/Methods.groovy
@@ -0,0 +1,21 @@
+def myMethod()
+{
+	println "Welcome"
+}
+myMethod()
+println "=========="
+def sum (int a, int b)
+{
+	println "Sum is " + (a+b)
+}
+
+sum(20, 30)
+println "=========="
+
+def  sub (int a, int b) {
+	int c = a-b;
+	return c;
+}
+
+def result = sub(50, 20)
+println result
---
+++ src/Methodstwo.groovy
@@ -0,0 +1,16 @@
+
+class Methodstwo {
+
+	static void main(args) {
+		// TODO Auto-generated method stub
+	Methodstwo obj = new Methodstwo();
+	println obj.myname()
+	
+		
+	}
+	
+	def String myname ()
+	{
+		return "Chay"
+	}
+}
